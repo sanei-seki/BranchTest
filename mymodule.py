a7799142e4a564942024205e662b472c73aea329
sanei-seki <156033885+sanei-seki@users.noreply.github.com>
2025-05-12 08:08:12 +0900

    ブランチテスト

---
+++ mymodule.py
@@ -0,0 +1,17 @@
+global_variable = "This is grlobal variable"
+
+def myfunc():
+    print("This is my function!!")
+
+
+def anotherfunc():
+    print("This is another function!!")
+
+
+#
+if __name__ == "__main__":
+    myfunc()
+    anotherfunc()
+    print("This is mymodulue!!")
+# def _internal_use_only():
+    
